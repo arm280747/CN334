8435e061590cdc39d8ff987cf6003b87991ccdc2
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2026-04-21 04:20:23 +0700

    Add LoginApp and AdminApp

--- TUBooking/TUBooking/urls.py
+++ TUBooking/TUBooking/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Bookingapp.urls'))
+    path("admin/", admin.site.urls),
+    path("", include("Bookingapp.urls")),
+    path("", include("LoginApp.urls")),
 ]
