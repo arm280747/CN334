d4457f3ad498decc91e2ef2239cf522e9733cf09
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2026-04-21 04:21:01 +0700

    Add booked to path

--- TUBooking/Bookingapp/urls.py
+++ TUBooking/Bookingapp/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Login, name="login"),
-    path('login', views.Login, name="login"),
-    path('logout', views.Logout, name="logout"),
-    path('home', views.Home, name="home"),
-    path('booking', views.Booking, name="booking"),
-    path('confirmbooking/<str:id>', views.Confirmbooking, name="confirmbooking"),
+    path("home/", views.Home, name="home"),
+    path("booking/", views.BookingView, name="booking"),
+    path(
+        "confirmbooking/<path:id>/",
+        views.Confirmbooking,
+        name="confirmbooking",
+    ),
+    path("booked/", views.Booked, name="booked"),
 ]
