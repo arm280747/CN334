d00b6e6b716e25d64de03df5383f5c78731e742a
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2026-04-21 04:07:02 +0700

    Register model to django admin

--- TUBooking/Bookingapp/admin.py
+++ TUBooking/Bookingapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Room, BookingForSubject, BookingForTutoring
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(BookingForSubject)
+admin.site.register(BookingForTutoring)
