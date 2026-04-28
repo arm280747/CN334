7752bbf1fe5dc9f2f85d9a4b5dde740a5e13fa9a
6610685031 <6610685031@student.tu.ac.th>
2026-04-28 16:44:47 +0700

    register blackout in admin and generate the migration

--- TUBooking/Bookingapp/admin.py
+++ TUBooking/Bookingapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Room, BookingForSubject, BookingForTutoring
+from .models import User, Room, BookingForSubject, BookingForTutoring, Blackout
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(BookingForSubject)
 admin.site.register(BookingForTutoring)
+admin.site.register(Blackout)
